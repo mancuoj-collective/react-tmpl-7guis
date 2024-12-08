18f8d9b49fac29836feebf79e8bd4500d9b0fdbe
mancuoj <mancuoj@gmail.com>
2024-12-09 00:03:47 +0800

    style: lighter border in dark mode

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 export function Wrapper({ title, children }: PropsWithChildren<{ title: string }>) {
   return (
-    <div className="flex flex-col rounded-lg border shadow-sm">
+    <div className="flex flex-col rounded-lg border shadow">
       <h1 className="border-b p-2.5 text-center text-sm text-muted-foreground">{title}</h1>
       {children}
     </div>
